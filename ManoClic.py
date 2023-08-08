4fbd511742a2e357836702e2d09008f9b1a74dbc
eucrpinc <97648726+eucrpinc@users.noreply.github.com>
2023-08-07 22:09:58 -0500

    ManoClic2.0.py
    Ahora al momento de correr el programa se abre el juego del Dino y se divide la pantalla

--- ManoClic.py
+++ ManoClic.py
@@ -1,71 +1,132 @@
-# Importar las librerías necesarias
 import mediapipe as mp
 import pyautogui
 import cv2
+import webbrowser
 
-# Inicializar los módulos de detección y dibujo de puntos y conexiones de la mano
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-# Configurar la detección de manos, permitiendo detectar hasta 2 manos
-# y estableciendo la confianza mínima para detectar y rastrear la mano
+# Abrir la página web en el navegador
+webbrowser.open("https://dinorunner.com/es/")
+
+# Simular la pulsación de la tecla Windows (Super) y la flecha a la derecha al mismo tiempo
+pyautogui.hotkey('win', 'right')
+
+# Iniciar la detección de manos
 with mp_hands.Hands(
     max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
 
-    # Configurar la captura de video desde la cámara del dispositivo
-    cap = cv2.VideoCapture(0)
+    # Iniciar la captura de video
+    cap = cv2.VideoCapture(0) 
 
-    # Inicializar la variable que indicará si ambas manos están cerradas
+    # Inicializar la variable booleana
     hands_closed = False
 
-    # Iterar mientras la captura de video esté activa
     while cap.isOpened():
-        # Leer un fotograma del video
         success, image = cap.read()
         if not success:
+            print("No se pudo obtener la imagen")
             break
 
-           # Convertir el fotograma a RGB para que sea compatible con mediapipe
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            # Detectar las manos en el fotograma
-            results = hands.process(image)
+        # Convertir la imagen a RGB y detectar las manos
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
 
-            # Convertir el fotograma a BGR para que sea compatible con OpenCV
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # Dibujar los landmarks de las manos en la imagen
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-            # Si se detectó alguna mano en el fotograma
-            if results.multi_hand_landmarks:
-                # Dibujar los puntos y conexiones de la mano en el fotograma
-                for hand_landmarks in results.multi_hand_landmarks:
-                    mp_drawing.draw_landmarks(
-                        image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-
-            # Si se detectaron dos manos en el fotograma
+            # Obtener la posición de las manos si hay al menos 2 manos detectadas
             if len(results.multi_hand_landmarks) >= 2:
-                # Obtener los puntos de referencia de la mano izquierda y derecha
                 left_hand = results.multi_hand_landmarks[0].landmark
                 right_hand = results.multi_hand_landmarks[1].landmark
 
-                # Si el dedo índice de la mano izquierda está a la izquierda del dedo medio
-                # y el dedo índice de la mano derecha está a la derecha del dedo medio
+                # Verificar si ambas manos están cerradas
                 if left_hand[4].x < left_hand[3].x and right_hand[4].x > right_hand[3].x:
-                    # Si ambas manos no están cerradas, marcar que lo están
-                    if not hands_closed:
-                        hands_closed = True
-                # Si no se cumple la condición anterior
-                else:
-                    # Si ambas manos están cerradas, hacer un clic con el mouse y marcar que no lo están
-                    if hands_closed:
+                    if not hands_closed:  # Si las manos no estaban cerradas previamente
+                        hands_closed = True  # Marcar que las manos están cerradas
+                else:  # Si ambas manos no están cerradas
+                    if hands_closed:  # Si las manos estaban cerradas previamente
+                        # Hacer clic en la pantalla
                         pyautogui.click()
-                        hands_closed = False
+                        hands_closed = False  # Marcar que las manos están abiertas
 
-            # Mostrar el fotograma con la detección de manos y el estado del mouse
+        # Mostrar la imagen en una ventana
         cv2.imshow("Hands Detection", image)
 
-        # Si se presiona la tecla 'q', salir del bucle
+        # Salir del loop si se presiona la tecla 'q'
         if cv2.waitKey(5) & 0xFF == ord('q'):
-            break        
+            break
 
-    # Liberar los recursos de la captura de video y cerrar las ventanas abiertas
+    # Liberar recursos
+    cap.release()
+    cv2.destroyAllWindows()
+import mediapipe as mp
+import pyautogui
+import cv2
+import webbrowser
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+# Abrir la página web en el navegador
+webbrowser.open("https://dinorunner.com/es/")
+
+# Simular la pulsación de la tecla Windows (Super) y la flecha a la derecha al mismo tiempo
+pyautogui.hotkey('win', 'right')
+
+# Iniciar la detección de manos
+with mp_hands.Hands(
+    max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+
+    # Iniciar la captura de video
+    cap = cv2.VideoCapture(0) 
+
+    # Inicializar la variable booleana
+    hands_closed = False
+
+    while cap.isOpened():
+        success, image = cap.read()
+        if not success:
+            print("No se pudo obtener la imagen")
+            break
+
+        # Convertir la imagen a RGB y detectar las manos
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
+
+        # Dibujar los landmarks de las manos en la imagen
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+            # Obtener la posición de las manos si hay al menos 2 manos detectadas
+            if len(results.multi_hand_landmarks) >= 2:
+                left_hand = results.multi_hand_landmarks[0].landmark
+                right_hand = results.multi_hand_landmarks[1].landmark
+
+                # Verificar si ambas manos están cerradas
+                if left_hand[4].x < left_hand[3].x and right_hand[4].x > right_hand[3].x:
+                    if not hands_closed:  # Si las manos no estaban cerradas previamente
+                        hands_closed = True  # Marcar que las manos están cerradas
+                else:  # Si ambas manos no están cerradas
+                    if hands_closed:  # Si las manos estaban cerradas previamente
+                        # Hacer clic en la pantalla
+                        pyautogui.click()
+                        hands_closed = False  # Marcar que las manos están abiertas
+
+        # Mostrar la imagen en una ventana
+        cv2.imshow("Hands Detection", image)
+
+        # Salir del loop si se presiona la tecla 'q'
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
+
+    # Liberar recursos
     cap.release()
     cv2.destroyAllWindows()
